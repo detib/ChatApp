3d942cabccbf7731bbae64fbf5bc7bea45a6f6f0
detib <detibaholli@gmail.com>
2022-06-16 04:52:39 +0200

    database changes

--- config/database.php
+++ config/database.php
@@ -5,7 +5,13 @@
     define( "DB_PASS", "MuzHmV4ZdO" );
     define( "DB_NAME", "xx9tjDv0Fg" );
 
-    $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+    $host = "remotemysql.com";
+    $user = "xx9tjDv0Fg";
+    $pass = "MuzHmV4ZdO";
+    $db = "xx9tjDv0Fg";
+
+    // $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+    $conn = mysqli_connect($host, $user, $pass, $db);
 
     if ( !$conn ) {
       echo ( "Connection failed: " . mysqli_connect_error() );
